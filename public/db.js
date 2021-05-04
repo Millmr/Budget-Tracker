a3b1c6581a82f460d362d0ca15d47ab6f4be031a
Matthew Miller <matt.r.miller09@gmail.com>
2021-05-03 20:18:18 -0500

    fixed service worker

--- public/db.js
+++ public/db.js
@@ -2,59 +2,61 @@ let db;
 
 const request = window.indexedDB.open("BudgetDB", 1);
 
-request.onupgradeneeded = (event) => {
+request.onupgradeneeded = function (event) {
+
     const db = event.target.result;
-    const objectStore = db.createObjectStore("BudgetStore", { autoIncrement: true });
+    const BudgetStore = db.createObjectStore("BudgetDB", { autoIncrement: true })
 };
 
-request.onsuccess = (event) => {
+request.onsuccess = function (event) {
     db = event.target.result;
+
     if (navigator.onLine) {
-        checkDatabase();
+    checkDatabase();
     }
 };
 
-request.onerror = (event) => {
-    console.log("something is bad", event)
+request.onerror = function (event) {
+
+    console.log("Something is bad", event);
 };
 
 function saveRecord(record) {
 
-    const transaction = db.transaction(["BudgetStore"], "readwrite");
-    const budgetStore = transaction.objectStore("BudgetStore");
+    const db = request.result;
+    const transaction = db.transaction(["BudgetDB"], "readwrite");
+    const BudgetStore = transaction.objectStore("BudgetDB");
 
-    budgetStore.add(record);
+    BudgetStore.add(record)
 }
 
 function checkDatabase() {
 
-    const transaction = db.transaction(["BudgetStore"])
-    const budgetStore = transaction.objectStore("BudgetStore");
+    const transaction = db.transaction(["BudgetDB"], "readwrite");
+    const BudgetStore = transaction.objectStore("BudgetDB");
 
-    const getAll = budgetStore.getAll();
+    const getAll = BudgetStore.getAll();
 
-    getAll.onsuccess = () => {
-        if (getAll.result.length > 0) {
-            fetch('/api/transaction/bulk', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json',
-                },
-            })
-                .then((response) => response.json())
-                .then((res) => {
-                    if (res) {
-                        const transaction = db.transaction(["BudgetStore"], "readwrite");
-                        const store = transaction.objectStore("BudgetStore");
-                        store.clear();
-                    }
-                });
-        }
+    getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+        fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((response) => response.json())
+        .then(() => {
+
+            const transaction = db.transaction(["BudgetDB"], "readwrite");
+            const BudgetStore = transaction.objectStore("BudgetDB");
+
+            BudgetStore.clear();
+        });
+    }
     };
 }
 
-
 window.addEventListener('online', checkDatabase);
-
--- public/service-worker.js
+++ public/service-worker.js
@@ -7,54 +7,71 @@ const FILES_TO_CACHE = [
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
     '/db.js',
-    '/dbcompressed.js',
     '/index.html',
     '/index.js',
     '/styles.css'
 ];
 
-self.addEventListener("install",  (evt) => {
-    //
+self.addEventListener("install", function (evt) {
+    // pre cache image data
+    // pre cache all static assets
     evt.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => {
-            console.log("Your files were pre-cached successfully!");
-            console.log(cache);
-            return cache.addAll(FILES_TO_CACHE);
-        })
+      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
     );
-
-});
-
-
-self.addEventListener("activate", function (evt) {
+  
+    // tell the browser to activate this service worker immediately once it
+    // has finished installing
+    self.skipWaiting();
+  });
+  
+  // activate
+  self.addEventListener("activate", function(evt) {
     evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME) {
-                        console.log("Removing old cache data", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
     );
-
+  
     self.clients.claim();
-});
-
-
-self.addEventListener("fetch", function (evt) {
-
-    evt.respondWith(
-        caches.open(CACHE_NAME)
-        .then ((cache) => {
-            return cache.match(evt.request)
-            .then((response) => {
-                return response || fetch(evt.request)
+  });
+  
+  // fetch
+  self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
             })
-        })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    evt.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(evt.request).then(response => {
+          return response || fetch(evt.request);
+        });
+      })
     );
-})
-
+  });
+  
